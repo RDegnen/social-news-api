124b973c7e3988dc8aa48def0c35a141b4e1a777
Ross <rdegnen@gmail.com>
2016-07-07 14:48:27 -0400

    made email and username required on user table

--- migrations/20160516170457_users.js
+++ migrations/20160516170457_users.js
@@ -3,8 +3,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable( 'users', function ( table ) {
     table.increments('id').primary();
-    table.string('username').unique();
-    table.string('email').unique();
+    table.string('username').unique().notNullable();
+    table.string('email').unique().notNullable();
     table.string('token');
     table.string('passwordDigest').notNullable();
 
